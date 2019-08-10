153a9e9969520733675ad28b3b299e2b9142fd95
Restomak <38076002+Restomak@users.noreply.github.com>
2019-08-09 22:37:08 -0400

    cpps

--- Duck Hunt/Source/Lighting.cpp
+++ Duck Hunt/Source/Lighting.cpp
@@ -19,6 +19,12 @@ Lighting::Lighting()
 	//distanceFogColor = glm::vec3(0.294f, 0.78f, 0.98f); //Original duck hunt background
 	distanceFogColor = glm::vec3(0.663f, 0.749f, 0.792f); //Winter sky
 	distanceFogDetails = glm::vec3(16.0f, 64.0f, 1.0f);
+
+	lightView = glm::lookAt(-vec3(sunlight),		// Camera position
+							vec3(sunlight),			// Look towards this point
+							vec3(5.0f, 5.0f, -10.0f)// Up vector
+	);
+	lightProjection = glm::ortho<float>(-10, 10, -10, 10, -10, 20);
 }
 
 Lighting::Lighting(glm::vec4 sun, glm::vec3 sunColor, glm::vec4 back, glm::vec3 billboardColor, glm::vec3 gunColor, float amb, glm::vec3 ambColor, glm::vec3 fog, glm::vec3 fogDetails)
@@ -38,6 +44,13 @@ Lighting::Lighting(glm::vec4 sun, glm::vec3 sunColor, glm::vec4 back, glm::vec3 
 
 	distanceFogColor = fog;
 	distanceFogDetails = fogDetails;
+	
+
+	lightView = glm::lookAt(-vec3(sunlight),		// Camera position
+							vec3(sunlight),			// Look towards this point
+							vec3(5.0f, 5.0f, -10.0f)// Up vector
+	);
+	lightProjection = glm::ortho<float>(-10, 10, -10, 10, -10, 20);
 }
 
 
@@ -130,8 +143,10 @@ glm::mat4 Lighting::GetLightLookAt()
 							glm::vec3(0.0f, 0.0f, 0.0f),
 							glm::vec3(0.0f, 1.0f, 0.0f));*/
 
-	return glm::lookAt(-vec3(sunlight),		// Camera position
-						vec3(sunlight),	// Look towards this point
-						vec3(5.0f, 5.0f, -10.0f)		// Up vector
-	);
+	return  lightView; 
+}
+
+glm::mat4 Lighting::GetLightProjection()
+{
+	return lightProjection;
 }
